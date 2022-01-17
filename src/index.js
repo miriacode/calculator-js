a9d7d99d1a61f8973eb4559415a25a257d0a5b1d
miriacode <miriamacunaenciso@gmail.com>
2022-01-16 19:01:47 -0500

    Changes in CSS and in HTML, Adding JS initial files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded",() =>{
+
+
+    //Event Listeners
+    //Numbers
+    const numberButtons =[...document.querySelectorAll(".buttons__number")]
+    console.log(numberButtons)
+    numberButtons.forEach(button => {
+        button.addEventListener("click", () => {
+        //   calculator.appendNumber(button.innerText)
+        //   calculator.updateDisplay()
+        console.log(button)
+        })
+    })
+    //Operators
+
+    //Ex
+
+
+
+
+
+
+})
